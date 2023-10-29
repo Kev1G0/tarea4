6a5368a0f67ad81c46e2579f654a02b3dfa59c1c
Kev1G0 <144714653+Kev1G0@users.noreply.github.com>
2023-10-29 13:18:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 const psswShowHide = document.querySelectorAll(".showHidePw");
 const passw = document.querySelectorAll(".password");
 const boton = document.getElementById("loginButton");
 const loader = document.getElementById("loader");
 const mensaje = document.getElementById("message");
-const email = document.querySelector('.login input[type="text"]');
-const password = document.querySelector('.login input[type="password"]');
+const emailInput = document.querySelector('.login input[type="text"]');
+const passwordInput = document.querySelector('.login input[type="password"]');
 
 psswShowHide.forEach((icono, index) => {
     icono.addEventListener("click", () => {
@@ -25,21 +24,22 @@ boton.addEventListener("click", () => {
     const correctEmail = 'usuario@ejemplo.com';
     const correctPassword = '123456';
 
-    const email = email.value;
-    const password = password.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
 
     setTimeout(() => {
         loader.style.display = "none";
         if (email === correctEmail && password === correctPassword) {
-            mensaje.textContent = "¡Eres un crack papa, Inicio de sesión exitoso!";
+            mensaje.textContent = "que xopa, bienvenido CRACK, ¡exito al que lo vea!";
             mensaje.style.color = "green";
         } else {
             mensaje.style.color = "red";
-            email.style.borderColor = "red";
-            password.style.borderColor = "red";
+            emailInput.style.borderColor = "red";
+            passwordInput.style.borderColor = "red";
         }
     }, 3000);
 });
+
 
 
 
